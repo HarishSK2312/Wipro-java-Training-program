671fd49c949ea2f89be14c960c2d96bcfd24aa59
SK Harish <90037523+HarishSK2312@users.noreply.github.com>
2023-02-07 08:33:46 +0530

    Add files via upload

---
+++ greatervalue.java
@@ -0,0 +1,27 @@
+package java1;
+
+import java.util.Scanner;
+
+public class greatervalue {
+   public static void main(sting[]args);
+   {
+	   Scanner s1= new scanner(system.in);
+	   System.out.print("enter the first number");
+	   int n1 = s1.nextInt();
+	   System.out.print("enter the second number");
+	   int n2 =s1.nextInt();
+	   
+	   if(n>0)
+	  {
+	    System.out.print(n1 +"is the greatest number");
+	  }
+	   else
+	  {
+	    System.out.print(n2 +"is the greatest number");   
+	  }
+	   if(n1==n2)
+	  {
+	    System.out.print("both are equal");
+	  }
+   }
+}
