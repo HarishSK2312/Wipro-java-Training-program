2e1acc36fc80c06c2d9b2793aa4df4216d4c2d31
SK Harish <90037523+HarishSK2312@users.noreply.github.com>
2023-02-09 10:11:05 +0530

    Add files via upload

---
+++ alldigitcount.java
@@ -0,0 +1,8 @@
+package java1;
+
+public class alldigitcount {
+	 String str=Integer.toString(input1);
+	   return str.length();
+	  }
+	}
+
---
+++ specifiedrange.java
@@ -0,0 +1,20 @@
+package java1;
+import java.io.*;
+import java.util.*;
+
+public class specifiedrange {
+	int count=0;        
+	   int pcount=0         
+	  for(int i=input1;i<=input2;i++){   
+	      
+	  count=0;        
+	for(int j=2;j<=Math.sqrt(i);j++) {       
+	 
+	if(i%j==0)         
+	  count++;       
+	  }       
+	if(count==0)         
+	  pcount++;     
+	return count;
+	  }
+}
