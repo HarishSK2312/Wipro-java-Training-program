70b62d8c604bb6c374c2734cb6ecdf2018feb03c
SK Harish <90037523+HarishSK2312@users.noreply.github.com>
2023-02-11 09:55:07 +0530

    Update most frequently occurring digit.java

--- DAY6/most frequently occurring digit.java
+++ DAY6/most frequently occurring digit.java
@@ -1,44 +1,26 @@
+import java.io.*;
+import  java.util.*;
 class UserMainCode
 {
-public class Result{
- public final int output1;
- public final int output2;
- public Result(int out1,int out2)
- {
-  output1=out1;
-  output2=out2;
- }
-}
-public Result decreasingSeq(int[] input1,int input2)
+public int mostFrequentlyOccurringDigit(int[] input1,int input2)
 {
- int c1=0,c2=0,max=0;
-for(int i=0;i<input2-1;i++)
-        {
-            if(input1[i]>input1[i+1])
-            {
-                c1++;
-            }
-            if((input1[i]<input1[i+1] && c1!=0) || ((i==input2-2) && c1!=0))
-            {
-                if(max<c1)
-                {
-                    max=c1;
-                }
-                c2++;
-                c1=0;
-            }
-        }
-        max=max+1;
-        if(c2==0)
-        {
-            max=0;
-        }
-        if(input2==0)
-        {
-            max=0;
-            c2=0;
-        }
-        Result r1= new Result(c2,max);
-        return r1;
-}
-}
+               int[] arr=new int[10];
+  for(int i=0;i<input2;i++)
+  {
+    while(input1[i]!=0){
+    int rem=input1[i]%10;
+    arr[rem]++;
+    input1[i]/=10;
+   }
+ }
+ int max=0;
+ int higest_occur_number=0;
+ for(int i=0;i<10;i++)
+ {
+  if(arr[i]>=max)
+  {max=arr[i];
+    higest_occur_number=i;
+  }
+ }
+ return higest_occur_number;
+}}
